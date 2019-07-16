b202402b3e51a21ad04455c11bb5bf0ac3497daa
zsfann <52970513+zsfann@users.noreply.github.com>
2019-07-16 20:07:56 +0200

    Add files via upload

---
+++ miosix-receiving-data/miosix/arch/common/core/mpu_cortexMx.d
@@ -0,0 +1,47 @@
+arch/common/core/mpu_cortexMx.o: arch/common/core/mpu_cortexMx.cpp \
+ arch/common/core/mpu_cortexMx.h config/miosix_settings.h \
+ arch/cortexM4_stm32f4/common/arch_settings.h \
+ config/arch/cortexM4_stm32f4/stm32f401re_nucleo/board_settings.h \
+ util/version.h interfaces/arch_registers.h \
+ arch/cortexM4_stm32f4/stm32f401re_nucleo/interfaces-impl/arch_registers_impl.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ arch/common/CMSIS/Include/core_cm4.h \
+ arch/common/CMSIS/Include/core_cmInstr.h \
+ arch/common/CMSIS/Include/cmsis_gcc.h \
+ arch/common/CMSIS/Include/core_cmFunc.h \
+ arch/common/CMSIS/Include/core_cmSimd.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+
+arch/common/core/mpu_cortexMx.h:
+
+config/miosix_settings.h:
+
+arch/cortexM4_stm32f4/common/arch_settings.h:
+
+config/arch/cortexM4_stm32f4/stm32f401re_nucleo/board_settings.h:
+
+util/version.h:
+
+interfaces/arch_registers.h:
+
+arch/cortexM4_stm32f4/stm32f401re_nucleo/interfaces-impl/arch_registers_impl.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+arch/common/CMSIS/Include/core_cm4.h:
+
+arch/common/CMSIS/Include/core_cmInstr.h:
+
+arch/common/CMSIS/Include/cmsis_gcc.h:
+
+arch/common/CMSIS/Include/core_cmFunc.h:
+
+arch/common/CMSIS/Include/core_cmSimd.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ miosix-receiving-data/miosix/arch/common/drivers/serial_stm32.d
@@ -0,0 +1,140 @@
+arch/common/drivers/serial_stm32.o: arch/common/drivers/serial_stm32.cpp \
+ arch/common/drivers/serial_stm32.h filesystem/console/console_device.h \
+ config/miosix_settings.h arch/cortexM4_stm32f4/common/arch_settings.h \
+ config/arch/cortexM4_stm32f4/stm32f401re_nucleo/board_settings.h \
+ util/version.h filesystem/devfs/devfs.h filesystem/file.h \
+ kernel/intrusive.h interfaces/atomic_ops.h \
+ arch/common/core/atomic_ops_impl_cortexMx.h interfaces/arch_registers.h \
+ arch/cortexM4_stm32f4/stm32f401re_nucleo/interfaces-impl/arch_registers_impl.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ arch/common/CMSIS/Include/core_cm4.h \
+ arch/common/CMSIS/Include/core_cmInstr.h \
+ arch/common/CMSIS/Include/cmsis_gcc.h \
+ arch/common/CMSIS/Include/core_cmFunc.h \
+ arch/common/CMSIS/Include/core_cmSimd.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ filesystem/stringpart.h kernel/sync.h kernel/kernel.h \
+ interfaces/portability.h arch/common/core/memory_protection.h \
+ arch/common/core/mpu_cortexMx.h \
+ arch/cortexM4_stm32f4/common/interfaces-impl/portability_impl.h \
+ kernel/scheduler/sched_types.h \
+ kernel/scheduler/priority/priority_scheduler_types.h \
+ kernel/scheduler/control/control_scheduler_types.h \
+ kernel/scheduler/control/parameters.h \
+ kernel/scheduler/edf/edf_scheduler_types.h \
+ stdlib_integration/libc_integration.h \
+ stdlib_integration/libstdcpp_integration.h kernel/error.h kernel/queue.h \
+ kernel/error.h interfaces/gpio.h \
+ arch/cortexM4_stm32f4/common/interfaces-impl/gpio_impl.h \
+ kernel/scheduler/scheduler.h \
+ kernel/scheduler/priority/priority_scheduler.h \
+ kernel/scheduler/priority/priority_scheduler_types.h kernel/kernel.h \
+ kernel/scheduler/control/control_scheduler.h \
+ kernel/scheduler/control/control_scheduler_types.h \
+ kernel/scheduler/edf/edf_scheduler.h \
+ kernel/scheduler/edf/edf_scheduler_types.h filesystem/ioctl.h \
+ arch/common/core/cache_cortexMx.h
+
+arch/common/drivers/serial_stm32.h:
+
+filesystem/console/console_device.h:
+
+config/miosix_settings.h:
+
+arch/cortexM4_stm32f4/common/arch_settings.h:
+
+config/arch/cortexM4_stm32f4/stm32f401re_nucleo/board_settings.h:
+
+util/version.h:
+
+filesystem/devfs/devfs.h:
+
+filesystem/file.h:
+
+kernel/intrusive.h:
+
+interfaces/atomic_ops.h:
+
+arch/common/core/atomic_ops_impl_cortexMx.h:
+
+interfaces/arch_registers.h:
+
+arch/cortexM4_stm32f4/stm32f401re_nucleo/interfaces-impl/arch_registers_impl.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+arch/common/CMSIS/Include/core_cm4.h:
+
+arch/common/CMSIS/Include/core_cmInstr.h:
+
+arch/common/CMSIS/Include/cmsis_gcc.h:
+
+arch/common/CMSIS/Include/core_cmFunc.h:
+
+arch/common/CMSIS/Include/core_cmSimd.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+arch/common/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+filesystem/stringpart.h:
+
+kernel/sync.h:
+
+kernel/kernel.h:
+
+interfaces/portability.h:
+
+arch/common/core/memory_protection.h:
+
+arch/common/core/mpu_cortexMx.h:
+
+arch/cortexM4_stm32f4/common/interfaces-impl/portability_impl.h:
+
+kernel/scheduler/sched_types.h:
+
+kernel/scheduler/priority/priority_scheduler_types.h:
+
+kernel/scheduler/control/control_scheduler_types.h:
+
+kernel/scheduler/control/parameters.h:
+
+kernel/scheduler/edf/edf_scheduler_types.h:
+
+stdlib_integration/libc_integration.h:
+
+stdlib_integration/libstdcpp_integration.h:
+
+kernel/error.h:
+
+kernel/queue.h:
+
+kernel/error.h:
+
+interfaces/gpio.h:
+
+arch/cortexM4_stm32f4/common/interfaces-impl/gpio_impl.h:
+
+kernel/scheduler/scheduler.h:
+
+kernel/scheduler/priority/priority_scheduler.h:
+
+kernel/scheduler/priority/priority_scheduler_types.h:
+
+kernel/kernel.h:
+
+kernel/scheduler/control/control_scheduler.h:
+
+kernel/scheduler/control/control_scheduler_types.h:
+
+kernel/scheduler/edf/edf_scheduler.h:
+
+kernel/scheduler/edf/edf_scheduler_types.h:
+
+filesystem/ioctl.h:
+
+arch/common/core/cache_cortexMx.h:
